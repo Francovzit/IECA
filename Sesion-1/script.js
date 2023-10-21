64793332c7c7f41c86cdc95d0c3aa4051c1bf655
Francovzit <francogvit@gmail.com>
2023-10-21 10:14:29 -0600

    Initial commit

---
+++ Sesion-1/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.querySelector("#input");
+  const button = document.querySelector("#button");
+  const list = document.querySelector("#list");
+
+  button.addEventListener("click", () => {
+    const li = document.createElement("li");
+    li.textContent = input.value;
+
+    list.appendChild(li);
+  });
+});
